9bb148bc62f385e4dcb838bc176af2c9376221f7
IGonjal <kaostias@gmail.com>
2015-12-15 15:56:09 +0100

    Update EventosGuerra.java

--- BePolitics/src/main/java/es/fdi/iw/model/pais/eventos/EventosGuerra.java
+++ BePolitics/src/main/java/es/fdi/iw/model/pais/eventos/EventosGuerra.java
@@ -1,5 +1,10 @@
 package pais.eventos;
-
+/**
+ * Contiene una lista de eventos pasados y un evento actual. Deberá poder resolver eventos
+ * 
+ * @author Ismael
+ *
+ */
 public class EventosGuerra {
 
 }
